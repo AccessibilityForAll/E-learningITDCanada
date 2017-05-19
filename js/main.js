70c621dd192430502d6fd3ceec66ee73b3900851
Raphael <raphaelsantosdesigner@gmail.com>
2017-05-19 16:58:22 -0700

    slide description

--- js/main.js
+++ js/main.js
@@ -46,14 +46,43 @@ $( document ).ready(function() {
     })*/
 
 	//Function page details Course menu
-
 	var triggerMenuDetailsCourse = $(".item-list-description li");
 	var containerMenuDetails = $(".item-list-description");
+
+	var triggerSlideContainer = $(".item-list-description li a");
+
+	var itemSlideContainer = $(".container-list");
 
 	triggerMenuDetailsCourse.on('click',function(){
 		event.preventDefault();
 		containerMenuDetails.find( "li" ).removeClass("active");
 		$(this).addClass("active");
+	})
+
+	triggerSlideContainer.on('click',function(){
+		event.preventDefault();
+		var atrDataItem = $(this).attr("data-name-item");
+
+		///////////////////////////////////////
+
+		switch(atrDataItem) {
+		    case "overview":
+		        itemSlideContainer.find( "li" ).removeClass("displayBlock");
+				$(".container-list .Overview-item").addClass("displayBlock");
+		        break;
+		    case "courseOutline":
+		         itemSlideContainer.find( "li" ).removeClass("displayBlock");
+				$(".container-list .course_outline").addClass("displayBlock");
+		        break;
+		    case "curricularGrade":
+		         itemSlideContainer.find( "li" ).removeClass("displayBlock");
+				$(".container-list .CurricularGrade").addClass("displayBlock");
+		        break;
+		    case "personalInformation":
+		         itemSlideContainer.find( "li" ).removeClass("displayBlock");
+				$(".container-list .personalInformations").addClass("displayBlock");
+		        break;
+		}
 	})
 
 });
